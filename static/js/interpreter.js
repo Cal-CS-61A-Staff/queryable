173cfd774851d5e9cb5af57c1e3686a12d4f0093
Paul Bitutsky <pbitutsky@users.noreply.github.com>
2019-04-21 23:27:54 -0700

    Beautify the SQL interpreter by adding a lot of CSS changes (#15)
    * Do not show visualizePane until necessary
    
    * Add Bootstrap
    
    * Add spinner
    
    * Make a lot of things beautiful
    
    * Update static/index.html
    
    Co-Authored-By: pbitutsky <pbitutsky@users.noreply.github.com>
    
    * Update static/js/execution.js
    
    Co-Authored-By: pbitutsky <pbitutsky@users.noreply.github.com>
    
    * Make links open in new tab

--- static/js/interpreter.js
+++ static/js/interpreter.js
@@ -1,12 +1,12 @@
 import {execute} from "./execution.js";
 
 const introText =
-`61A SQL Web Interpreter
-<a href="https://github.com/Cal-CS-61A-Staff/queryable">on GitHub</a>
+`<b><a href="https://cs61a.org" target="_blank">CS 61A</a> SQL Web Interpreter</b>
 --------------------------------------------------------------------------------
-Welcome to the 61A SQL interpreter!
+Welcome to the 61A SQL interpreter! 
 Type <code>.help</code> for instructions, or <code>.read</code> to load a file from your computer.
 The tables used in homework, labs, and lecture are already available to use.
+Check out the code for this app on <a href="https://github.com/Cal-CS-61A-Staff/queryable" target="_blank">GitHub</a>.
 `;
 
 function initializeAce(editorDiv) {
